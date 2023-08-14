aaeb3642569c396820f71a5ff5f0df991aa33cb9
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-08-14 18:02:07 +0800

    這是一隻大豬成功

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # [Music Player](https://spanion-music.netlify.app/)
 
+VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+need FFMPEG
+UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+
 A simple PWA to store and play local music files.
 
 ![https://github.com/AKAspanion/chat-app/blob/master/img/sr.gif?raw=true](https://github.com/AKAspanion/music-app/blob/master/img/sr.gif?raw=true)
--- src/speech/index.ts
+++ src/speech/index.ts
@@ -1,1 +1,17 @@
-/*do not modify this file, created by preval*/const howhow :Function = (words:string)=>{return words;}; export default howhow;
+/*do not modify this file, created by preval*/
+import { sha256 } from '@noble/hashes/sha256'; 
+import { bytesToHex as toHex } from '@noble/hashes/utils';
+
+interface Bot {
+    [key: string]: string;
+}
+//e
+const bot: Bot = {
+    fba49a0af80691c092eafd5cf588d76ab1e5dd31a87622bfdb7018bb77c04fcf282d21080527734e1aeff8d73a4610da9244c3b39ee703518af795b5f6fc621: "zhe4 shi4 yi1 zhi1 da4 zhu1"
+};
+
+const howhow: (words: string) => string = (words) => {
+    return bot[toHex(sha256(words))];
+};
+
+export default howhow;
